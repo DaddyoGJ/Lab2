3e8bce3144511deb1bb6dbc68d680de091920786
VuongQuanKiet <quankiet.22@ichat.sp.edu.sg>
2024-10-30 10:25:22 +0800

    Exercise 2

--- lab2.py
+++ lab2.py
@@ -1,1 +1,21 @@
-print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+def main():
+    print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+    display_main_menu()
+    num_list = get_user_input()
+
+def display_main_menu():
+    print("display_main_menu")
+
+def get_user_input():  
+
+def calc_average():
+    print("calc_average")    
+
+def find_min_max():
+
+def sort_temperature():
+
+def calc_median_temperature():
+
+if __name__ == "__main__":
+    main()
