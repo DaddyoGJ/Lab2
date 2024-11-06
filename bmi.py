e4626b060847a680852fbba3db5ae3fe4253959c
VuongQuanKiet <quankiet.22@ichat.sp.edu.sg>
2024-11-06 12:36:00 +0800

    Ex 1

--- bmi.py
+++ bmi.py
@@ -1,14 +1,19 @@
 def calculate_bmi(height, weight):
  print("Height = " + str(height))
  print("Weight = " + str(weight))
+
  bmi = (weight/(height*height))
+ 
  print("BMI = " + str(bmi))
+
  if bmi < 18.5:
   print("Under Weight")
   return -1
+ 
  elif 18.5 <= bmi <= 25.0:
   print("Normal Weight")
   return 0
+ 
  elif bmi > 25.0:
   print("Over Weight")
   return 1
