5a3e5c705a9c7f6837752935b826299125a91203
VuongQuanKiet <quankiet.22@ichat.sp.edu.sg>
2024-11-06 12:36:00 +0800

    Ex 1

---
+++ Test_Lab2.py
@@ -0,0 +1,22 @@
+import Lab2.lab2 as lab2
+
+def test_find_min_max():
+    inputno = [70, 50, 80, 90, 100]
+    result = lab2.sort_temperature(inputno)
+    min = result[0]
+    max = result [-1]
+
+    assert (min == 50)
+    assert (max == 100)
+
+def test_calc_average():
+    inputno = [70, 50, 80, 90, 100]
+    result = lab2.calc_average(inputno)
+
+    assert (result == 78)
+
+def test_calc_median_temperature():
+    inputno = [70, 50, 80, 90, 100]
+    result = lab2.calc_median_temperature(inputno)
+    
+    assert (result == 80)
